eea08099fd17973fc048d471d887a3179426655d
Bianca Padilla <biancalpadilla@gmail.com>
2015-10-14 18:30:24 -0400

    First commit

---
+++ stockpicker.rb
@@ -0,0 +1,11 @@
+def stockpicker
+    response = HTTParty.get("http://finance.yahoo.com/q?s=aapl")
+    dom = Nokogiri::HTML(response.body)
+    stock_price = dom.css(".time_rtq_ticker span").first
+    company = dom.css(".title h2").first
+    company.content + " Current stock price $" + stock_price.content
+end
+
+
+
+
